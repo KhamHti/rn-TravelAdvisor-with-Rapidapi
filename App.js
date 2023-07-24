4916d8fd3427fa0ea0d2cd3ec3c5fe52295ab1fd
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-07-24 22:01:36 +0630

    second

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, Text, View } from 'react-native';
-import { TailwindProvider } from 'tailwindcss-react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from './screens/HomeScreen';
-import Discover from './screens/Discover';
-import ItemScreen from './screens/ItemScreen';
+import HomeScreen from "./screens/HomeScreen";
+import Discover from "./screens/Discover";
+import ItemScreen from "./screens/ItemScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,13 +13,11 @@ export default function App() {
     <TailwindProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name='Home' component={HomeScreen} />
-          <Stack.Screen name='Discover' component={Discover} />
-          <Stack.Screen name='ItemScreen' component={ItemScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Discover" component={Discover} />
+          <Stack.Screen name="ItemScreen" component={ItemScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </TailwindProvider>
   );
 }
-
-
